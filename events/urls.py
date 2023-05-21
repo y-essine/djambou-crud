d45f9feb7abcabd0d32e6c65f30d06b2c2d37b86
YESSINE <prodtf@gmail.com>
2023-05-21 20:25:33 +0100

    all

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('events/', views.index, name='index'),
     path('api/events/', views.all, name='all'),
+    path('api/events/<int:id>', views.get, name='get'),
     path('api/events/add', views.add, name='add'),
+    path('api/events/update/<int:id>', views.update, name='update'),
+    path('api/events/delete/<int:id>', views.delete, name='delete'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# import json to return json response
-import json
-# import the Event model and return all events
+
 from .models import Event
+from .serializers import EventSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 def index(request):
     return HttpResponse("Hello events!");
 
+@api_view(['GET'])
 def all(request):
     events = Event.objects.all()
-    return HttpResponse(events.to_json(), status=200)
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-def add(request, body):
-    event = Event.objects.create(body=body)
-    return HttpResponse({'event': event.to_dict()})
+@api_view(['GET'])
+def get(request, id):
+    try:
+        event = Event.objects.get(id=id)
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def add(request):
+    serializer = EventSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update(request, id):
+    try:
+        event = Event.objects.get(id=id)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete(request, id):
+    try:
+        event = Event.objects.get(id=id)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Event.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
